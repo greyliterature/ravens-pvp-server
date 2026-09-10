334c88765d443ed51786f56f354250349ae6ee33
scripture <125283797+greyliterature@users.noreply.github.com>
2026-09-10 11:38:04 -0400

    Fix trying to get gamemode table to early

--- addons/rv_extra/lua/autorun/client/rv_limithit.lua
+++ addons/rv_extra/lua/autorun/client/rv_limithit.lua
@@ -1,12 +1,14 @@
-local GMMETA = gmod.GetGamemode()
-function GMMETA:LimitHit(name) -- from https://github.com/Facepunch/garrysmod/blob/2ab95e5342f831ade3d9768a8d5c684b542d2d2e/garrysmod/gamemodes/sandbox/gamemode/cl_init.lua#L32
-    local str = "#SBoxLimit_" .. name
-    local translated = language.GetPhrase(str)
-    if str == translated then
-        -- No translation available, apply our own
-        translated = language.FormatPhrase("hint.hitXlimit", language.GetPhrase(name))
-    end
+hook.Add("OnGamemodeLoaded", "ChangeLimitHit", function()
+    local GMMETA = gmod.GetGamemode()
+    function GMMETA:LimitHit(name) -- from https://github.com/Facepunch/garrysmod/blob/2ab95e5342f831ade3d9768a8d5c684b542d2d2e/garrysmod/gamemodes/sandbox/gamemode/cl_init.lua#L32
+        local str = "#SBoxLimit_" .. name
+        local translated = language.GetPhrase(str)
+        if str == translated then
+            -- No translation available, apply our own
+            translated = language.FormatPhrase("hint.hitXlimit", language.GetPhrase(name))
+        end
 
-    self:AddNotify(translated, NOTIFY_ERROR, 6)
-    surface.PlaySound("buttons/combine_button_locked.wav")
-end
+        self:AddNotify(translated, NOTIFY_ERROR, 6)
+        surface.PlaySound("buttons/combine_button_locked.wav")
+    end
+end)
